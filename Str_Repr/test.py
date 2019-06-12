5953e359e12bfde9fc55284640a33842e545f61b
Deepak Kumar <deepakmotu@gmail.com>
2019-06-12 07:15:12 +0530

    Adding Corey Snippets

--- Str_Repr/test.py
+++ Str_Repr/test.py
@@ -5,12 +5,10 @@ a = datetime.datetime.utcnow().replace(tzinfo=pytz.UTC)
 
 b = str(a)
 
-print 'str(a): {}'.format(str(a))
-print 'str(b): {}'.format(str(b))
+print('str(a): {}'.format(str(a)))
+print('str(b): {}'.format(str(b)))
 
-print
 
-print 'repr(a): {}'.format(repr(a))
-print 'repr(b): {}'.format(repr(b))
+print('repr(a): {}'.format(repr(a)))
+print('repr(b): {}'.format(repr(b)))
 
-print
